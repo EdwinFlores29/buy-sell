c1a1e1a8980cffb151787eaf1e8281f8a4b0bf24
Edwin Oswaldo Flores Gutierrez <46845817+EdwinFlores29@users.noreply.github.com>
2024-12-23 14:02:07 -0600

    funcionando los links
    a través de detalle del producto

--- src/app/listings-detail-page/listings-detail-page.component.ts
+++ src/app/listings-detail-page/listings-detail-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Listing} from "../types";
+import { fakeListings} from "../fake-data";
 
 @Component({
   selector: 'app-listings-detail-page',
@@ -6,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./listings-detail-page.component.css']
 })
 export class ListingsDetailPageComponent {
+  listings: Listing[] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+  ) {}
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.listings = fakeListings.filter(listing => listing.id === id);
+  }
 
 }
