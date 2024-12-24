69821ee02700eb877fc3884f2ee1aac007c2ba7f
Edwin Oswaldo Flores Gutierrez <46845817+EdwinFlores29@users.noreply.github.com>
2024-12-23 18:20:34 -0600

    update la lista de productos
    my-listings

--- src/app/my-listings-page/my-listings-page.component.ts
+++ src/app/my-listings-page/my-listings-page.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Listing} from "../types";
+import { fakeMyListings } from "../fake-data";
 
 @Component({
   selector: 'app-my-listings-page',
   templateUrl: './my-listings-page.component.html',
   styleUrls: ['./my-listings-page.component.css']
 })
-export class MyListingsPageComponent {
+export class MyListingsPageComponent implements OnInit {
+  listings: Listing[] = [];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.listings = fakeMyListings;
+  }
+  onDeleteClicked(listingId: string) {
+    alert(`Deleting your listing ${listingId} has been deleted!`);
+  }
 
 }
