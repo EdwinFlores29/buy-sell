c46e88364c8bc22624baa0098e0ed48b8b73b1e0
Edwin Oswaldo Flores Gutierrez <46845817+EdwinFlores29@users.noreply.github.com>
2024-12-24 14:00:30 -0600

    mensajes de error
    con el modulo @hapi/boom

--- buy-sell-backend/src/routes/getListing.js
+++ buy-sell-backend/src/routes/getListing.js
@@ -1,10 +1,14 @@
-import { fakeListings} from "./fake-data";
+const Boom = require('@hapi/boom');
+import { fakeListings } from "./fake-data";
 
 export const getListingRoute =  {
     method: 'GET',
     path: '/api/listings/{id}',
     handler: (request, h) => {
+
       const id = request.params.id;
-        return fakeListings.find(listing => listing.id === id);
+        const listing = fakeListings.find(listing => listing.id === id);
+        if (!listing) throw Boom.notFound(`Listing with id ${id} not found`);
+        return listing;
     }
 };
