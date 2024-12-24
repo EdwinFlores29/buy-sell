339867b01c05cd2e4eb241966ed4b27915025ca8
Edwin Oswaldo Flores Gutierrez <46845817+EdwinFlores29@users.noreply.github.com>
2024-12-23 20:05:54 -0600

    update sobre agregar productos
    my-listings, sobre agregar los productos

--- src/app/new-listings-page/new-listings-page.component.ts
+++ src/app/new-listings-page/new-listings-page.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-new-listings-page',
   templateUrl: './new-listings-page.component.html',
   styleUrls: ['./new-listings-page.component.css']
 })
-export class NewListingsPageComponent {
+export class NewListingsPageComponent implements OnInit {
+  name: string = '';
+  description: string = '';
+  price: string = '';
+
+  constructor(
+    private router: Router,
+  ) { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(): void {
+     alert('New Listing Created!');
+    this.router.navigateByUrl('/my-listings');
+  }
 
 }
