0221368af5cd7879f686ea0c3bfd0042bbe868cd
Edwin Oswaldo Flores Gutierrez <46845817+EdwinFlores29@users.noreply.github.com>
2024-12-26 10:33:50 -0600

    por medio de post actualizar
    updaListing.js

--- buy-sell-backend/src/routes/index.js
+++ buy-sell-backend/src/routes/index.js
@@ -3,12 +3,13 @@ import { getListingRoute} from "./getListing";
 import { addViewToListingRoute} from "./addViewToListing";
 import { getUserListingsRoute} from "./getUserListings";
 import { createNewListingRoute} from "./createNewListing";
-
+import { updateListingRoute} from "./updateListing";
 export default [
   getAllListingsRoute,
   getListingRoute,
   addViewToListingRoute,
   getUserListingsRoute,
   createNewListingRoute,
+  updateListingRoute,
   ];
 
