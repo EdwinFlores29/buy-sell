5cdf50f7b02422e9cb980c1bedf41893e10bb39c
Edwin Oswaldo Flores Gutierrez <46845817+EdwinFlores29@users.noreply.github.com>
2024-12-25 22:02:11 -0600

    aumentar las vistas
    en el archivo addViewToListig

--- buy-sell-backend/src/database.js
+++ buy-sell-backend/src/database.js
@@ -1,31 +1,20 @@
 import mysql from 'mysql';
+
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'edwinf',
     password: 'dariohakuna2*',
-    database: 'buy_sell'
- });
- export const db = {
-    connect: () => {
-        connection.connect((err) => {
-            if (err) {
-                console.error('error connecting: ' + err.stack);
-                return;
-            }
-            console.log('connected as id ' + connection.threadId);
-        });
-    },
-    query: (sql, args) => {
-        return new Promise((resolve, reject) => {
-            connection.query(sql, args, (err, results) => {
-                if (err) {
-                    return reject(err);
-                }
-                resolve({ results });
-            });
-        });
-    },
-    end: () => {
-        connection.end();
-    },
- }
+    database: 'buy_sell',
+});
+
+export const db = {
+    connect: () => connection.connect(),
+    query: (queryString, escapedValues) =>
+        new Promise((resolve, reject) => {
+            connection.query(queryString, escapedValues, (error, results, fields) => {
+                if (error) reject(error);
+                resolve({ results, fields });
+            })
+        }),
+    end: () => connection.end(),
+}
